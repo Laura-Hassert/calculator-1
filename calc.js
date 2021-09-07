a9163a19179226084c8cf4e5ef8b3f489e820e2d
Laura-Hassert <laura.hassert@gmail.com>
2021-09-07 17:48:40 -0400

    all functions

--- calc.js
+++ calc.js
@@ -29,12 +29,27 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === "/"){
         console.log(num1 / num2);
     }
-	
+
 	if (mathSymbol === "√"){
         console.log(Math.sqrt(num1));
     }  
 
+	if (mathSymbol === "sqr"){
+        console.log(Math.pow(num1, 2));
+    }  
+
+	if (mathSymbol === "cube"){
+        console.log(Math.cbrt(num1));
+    } 
+	 
+	if (mathSymbol === "^n"){
+        console.log(Math.pow(num1, num2));
+    } 
   
+	if (mathSymbol === "rem"){
+        console.log(num1 % num2);
+    } 
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
