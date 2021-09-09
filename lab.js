294adfd3d5cb943fb9d9cd691f553681bf4cc530
Laura-Hassert <laura.hassert@gmail.com>
2021-09-08 21:41:41 -0400

    all solutions

--- lab.js
+++ lab.js
@@ -177,14 +177,12 @@ let someNum = myFavoriteNumbers.slice(5)
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
-someNum = myFavoriteNumbers.slice(7)
-for (let i = 0; i < 1; i++) {
+
     if(myFavoriteNumbers.length < 7) {
     console.log("There are not enough elements in this array");
-    }
+    } else { someNum = myFavoriteNumbers.slice(7);
+        console.log(someNum);
 }
-console.log(someNum);
-
 
 ////////// PROBLEM 11 //////////
 
@@ -194,7 +192,12 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+let oddNumbers = []
+for (let i = 0; i < listOfNumbers.length; i++) {
+    if(listOfNumbers[i] % 3 === 0) {
+        oddNumbers.push(listOfNumbers[i]);
+    }
+} console.log(oddNumbers)
 
 
 ////////// PROBLEM 12 //////////
@@ -206,7 +209,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. Console-log every item in the array.
 */
 
-//Code Here
+for(let i = letters.length - 1; i >= 0; i--) {
+    console.log(letters[i]); 
+    } 
 
 
 ////////// Advanced Problems //////////
@@ -229,7 +234,25 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+switch(letterGrade) {
+    case 'A':
+      console.log("The student is doing excellently.");
+      break;
+    case 'B':
+      console.log("The student is doing well.");
+      break;
+    case 'C':
+        console.log("The student is doing alright.");
+        break;
+    case 'D':
+        console.log("The student is not doing very well.");
+        break;
+    case 'F':
+        console.log("The student is failing.");
+        break;
+    default:
+      console.log("Not an eligible grade.");
+}
 
 
 ////////// PROBLEM 14 //////////
@@ -257,3 +280,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for(let i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("Devmountain"); 
+    } else if(i % 3 === 0) {
+        console.log("Dev");
+    } else if(i % 5 === 0) {
+        console.log("mountain");
+    } else {
+        console.log(i);
+    }
+}
